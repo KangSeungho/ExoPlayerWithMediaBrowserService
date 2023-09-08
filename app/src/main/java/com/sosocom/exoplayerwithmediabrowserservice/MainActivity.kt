a1a4a621d2a3451780978b33c6b2e0a689b9d26c
ksh <sosocom95@naver.com>
2023-09-09 07:58:36 +0900

    - Activity에 MediaBrowser 연결

--- app/src/main/java/com/sosocom/exoplayerwithmediabrowserservice/MainActivity.kt
+++ app/src/main/java/com/sosocom/exoplayerwithmediabrowserservice/MainActivity.kt
@@ -1,11 +1,98 @@
 package com.sosocom.exoplayerwithmediabrowserservice
 
+import android.content.ComponentName
+import android.media.AudioManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mediaBrowser: MediaBrowserCompat
+
+    private lateinit var btnPlayPause: ImageView
+
+    private val connectionCallbacks = object: MediaBrowserCompat.ConnectionCallback() {
+        override fun onConnected() {
+            mediaBrowser.sessionToken.also { token ->
+                val mediaController = MediaControllerCompat(
+                    this@MainActivity,
+                    token
+                )
+
+                MediaControllerCompat.setMediaController(this@MainActivity, mediaController)
+
+                buildTransportControls()
+            }
+        }
+
+        override fun onConnectionSuspended() {
+
+        }
+
+        override fun onConnectionFailed() {
+
+        }
+    }
+
+    private val controllerCallback = object: MediaControllerCompat.Callback() {
+        override fun onMetadataChanged(metadata: MediaMetadataCompat?) {
+
+        }
+
+        override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
+
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mediaBrowser = MediaBrowserCompat(
+            this,
+            ComponentName(this, MediaPlaybackService::class.java),
+            connectionCallbacks,
+            null
+        )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mediaBrowser.connect()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        volumeControlStream = AudioManager.STREAM_MUSIC
+    }
+
+    override fun onStop() {
+        super.onStop()
+        MediaControllerCompat.getMediaController(this)?.unregisterCallback(controllerCallback)
+        mediaBrowser.disconnect()
+    }
+
+    private fun buildTransportControls() {
+        val mediaController = MediaControllerCompat.getMediaController(this)
+
+        btnPlayPause = findViewById<ImageView?>(R.id.play_pause).apply {
+            setOnClickListener {
+                val pbState = mediaController.playbackState.state
+                if(pbState == PlaybackStateCompat.STATE_PLAYING) {
+                    mediaController.transportControls.pause()
+                } else {
+                    mediaController.transportControls.play()
+                }
+            }
+        }
+
+        val metadata = mediaController.metadata
+        val pbState = mediaController.playbackState
+
+        mediaController.registerCallback(controllerCallback)
     }
 }
