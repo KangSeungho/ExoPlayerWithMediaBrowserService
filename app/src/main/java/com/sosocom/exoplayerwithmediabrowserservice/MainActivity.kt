e305d4f301955fd18c3dd98f848a96457a7b0e14
ksh <sosocom95@naver.com>
2023-09-09 11:19:44 +0900

    - Activity에 MediaBrowser 연결 및 AudioFocus 추가

--- app/src/main/java/com/sosocom/exoplayerwithmediabrowserservice/MainActivity.kt
+++ app/src/main/java/com/sosocom/exoplayerwithmediabrowserservice/MainActivity.kt
@@ -8,15 +8,21 @@ import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.PlaybackStateCompat
+import android.util.Log
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "EPMB_MAIN"
+    }
+
     private lateinit var mediaBrowser: MediaBrowserCompat
 
     private lateinit var btnPlayPause: ImageView
 
     private val connectionCallbacks = object: MediaBrowserCompat.ConnectionCallback() {
         override fun onConnected() {
+            Log.d(TAG, "connectionCallback onConnected")
             mediaBrowser.sessionToken.also { token ->
                 val mediaController = MediaControllerCompat(
                     this@MainActivity,
@@ -30,27 +36,28 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onConnectionSuspended() {
-
+            Log.d(TAG, "connectionCallback onConnectionSuspended")
         }
 
         override fun onConnectionFailed() {
-
+            Log.d(TAG, "connectionCallback onConnectionFailed")
         }
     }
 
     private val controllerCallback = object: MediaControllerCompat.Callback() {
         override fun onMetadataChanged(metadata: MediaMetadataCompat?) {
-
+            Log.d(TAG, "controllerCallback onMetadataChanged metadata = $metadata")
         }
 
         override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
-
+            Log.d(TAG, "controllerCallback onPlaybackStateChanged state = $state")
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate")
 
         mediaBrowser = MediaBrowserCompat(
             this,
@@ -63,15 +70,18 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         mediaBrowser.connect()
+        Log.d(TAG, "onStart and connect")
     }
 
     override fun onResume() {
         super.onResume()
         volumeControlStream = AudioManager.STREAM_MUSIC
+        Log.d(TAG, "onResume")
     }
 
     override fun onStop() {
         super.onStop()
+        Log.d(TAG, "onStop and disconnect")
         MediaControllerCompat.getMediaController(this)?.unregisterCallback(controllerCallback)
         mediaBrowser.disconnect()
     }
--- app/src/main/java/com/sosocom/exoplayerwithmediabrowserservice/MediaPlaybackService.kt
+++ app/src/main/java/com/sosocom/exoplayerwithmediabrowserservice/MediaPlaybackService.kt
@@ -1,9 +1,18 @@
 package com.sosocom.exoplayerwithmediabrowserservice
 
+import android.content.Context
+import android.content.Intent
+import android.media.AudioAttributes
+import android.media.AudioFocusRequest
+import android.media.AudioManager
+import android.media.AudioManager.OnAudioFocusChangeListener
+import android.os.Build
 import android.os.Bundle
+import android.service.media.MediaBrowserService
 import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import androidx.media.MediaBrowserServiceCompat
@@ -18,21 +27,82 @@ class MediaPlaybackService: MediaBrowserServiceCompat() {
         private const val MY_EMPTY_MEDIA_ROOT_ID = "empty_root_id"
     }
 
-    private var mediaSession: MediaSessionCompat? = null
+    private lateinit var afChangeListener: AudioManager.OnAudioFocusChangeListener
+    private lateinit var mediaSession: MediaSessionCompat
     private lateinit var stateBuilder: PlaybackStateCompat.Builder
+
+    private lateinit var audioFocusRequest: AudioFocusRequest
+
+    val focusLock = Any()
+
+    var playbackDelayed = false
+    var playbackNowAuthorized = false
 
     private val mediaSessionCallback = object: MediaSessionCompat.Callback() {
         override fun onPlay() {
+            Log.d(TAG, "mediaSessionCallback onPlay")
+            val am = baseContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
 
+            val result: Int
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+                audioFocusRequest = AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN).run {
+                    setOnAudioFocusChangeListener(afChangeListener)
+                    setAudioAttributes(AudioAttributes.Builder().run {
+                        setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                        build()
+                    })
+                    setAcceptsDelayedFocusGain(true)
+                    build()
+                }
+
+                result = am.requestAudioFocus(audioFocusRequest)
+            } else {
+                result = am.requestAudioFocus(
+                    afChangeListener,
+                    AudioManager.STREAM_MUSIC,
+                    AudioManager.AUDIOFOCUS_GAIN
+                )
+            }
+
+            synchronized(focusLock) {
+                playbackNowAuthorized = when(result) {
+                    AudioManager.AUDIOFOCUS_REQUEST_FAILED -> false
+                    AudioManager.AUDIOFOCUS_REQUEST_GRANTED -> {
+                        startService(Intent(baseContext, MediaBrowserService::class.java))
+                        mediaSession.isActive = true
+                        // 플레이어 시작
+                        true
+                    }
+                    AudioManager.AUDIOFOCUS_REQUEST_DELAYED -> {
+                        playbackDelayed = true
+                        false
+                    }
+                    else -> false
+                }
+            }
+        }
+
+        override fun onStop() {
+            Log.d(TAG, "mediaSessionCallback onStop")
+            val am = baseContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+                am.abandonAudioFocusRequest(audioFocusRequest)
+            }
+            mediaSession.isActive = false
+            // 플레이어 stop
         }
 
         override fun onPause() {
-
+            Log.d(TAG, "mediaSessionCallback onPause")
+            val am = baseContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+            // 플레이어 pause
         }
     }
 
     override fun onCreate() {
         super.onCreate()
+        Log.d(TAG, "onCreate")
 
         mediaSession = MediaSessionCompat(baseContext, TAG).apply {
             setFlags(MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS
@@ -49,6 +119,32 @@ class MediaPlaybackService: MediaBrowserServiceCompat() {
 
             setSessionToken(sessionToken)
         }
+
+        afChangeListener = OnAudioFocusChangeListener { focusChange ->
+            when (focusChange) {
+                AudioManager.AUDIOFOCUS_GAIN ->{
+                    if(playbackDelayed) {
+                        synchronized(focusLock) {
+                            playbackDelayed = false
+                        }
+                        // 재생
+                    }
+                }
+                AudioManager.AUDIOFOCUS_LOSS -> {
+                    synchronized(focusLock) {
+                        playbackDelayed = false
+                    }
+                    // 일시정지
+                }
+                AudioManager.AUDIOFOCUS_LOSS_TRANSIENT -> {
+                    synchronized(focusLock) {
+                        playbackDelayed = false
+                    }
+                    // 일시정지
+                }
+            }
+        }
+        initNotification(mediaSession)
     }
 
     override fun onGetRoot(
@@ -56,6 +152,7 @@ class MediaPlaybackService: MediaBrowserServiceCompat() {
         clientUid: Int,
         rootHints: Bundle?
     ): BrowserRoot {
+        Log.d(TAG, "onGetRoot clientPackageName = $clientPackageName, clientUid = $clientUid")
         return BrowserRoot(MY_MEDIA_ROOT_ID, null)
     }
 
@@ -63,6 +160,7 @@ class MediaPlaybackService: MediaBrowserServiceCompat() {
         parentId: String,
         result: Result<MutableList<MediaBrowserCompat.MediaItem>>
     ) {
+        Log.d(TAG, "onLoadChildren parendId = $parentId")
         if(MY_EMPTY_MEDIA_ROOT_ID == parentId) {
             result.sendResult(null)
             return
